61e3108cc0fa552c8be6186ed667a067647bba3d
Umar96 <umarjh96@gmail.com>
2020-03-29 04:08:13 +0500

    ISSUE#1: Remove mapWeb in RouteServiceProvider

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -44,8 +44,6 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
-
         //
     }
 
@@ -56,12 +54,6 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
-    }
 
     /**
      * Define the "api" routes for the application.
@@ -73,8 +65,8 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }
