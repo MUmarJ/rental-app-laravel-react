dd2140082e3ee919b3cc066604df54310075dcb4
Umar96 <umarjh96@gmail.com>
2020-05-03 12:40:30 +0500

    #3: Add Post Seeder for easy dummy data

---
+++ src/database/seeds/PostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create('en_US');
+        DB::table('posts')->insert([
+            'title' => $faker->name,
+            'description' => $faker->sentence,
+            'post_status' => 'ACTIVE',
+        ]);
+    }
+}
