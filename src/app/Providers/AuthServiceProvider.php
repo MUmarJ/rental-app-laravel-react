d7c78f83e4b7f8bb294fa96b977d35906a3ea910
Umar96 <umarjh96@gmail.com>
2020-05-10 23:59:12 +0500

    #4: Add Passport Routes + Middleware for token

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +25,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //Register default Passport routes
+        Passport::routes();
+        Passport::withoutCookieSerialization();
+
+        //Token Lifetimes
+        Passport::tokensExpireIn(now()->addDays(7));
+        Passport::refreshTokensExpireIn(now()->addDays(15));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(1));
     }
 }
